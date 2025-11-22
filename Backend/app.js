338fd725d5dafcbe46909ace94dbb5a4d115e76f
Omee Balar <omee005807@gmail.com>
2025-11-22 16:55:26 +0530

    fix backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(_, res)=>{
-    res.status(200).json({message:"Server is Running"})
+app.use(express.json())
+app.get('/', (_, res) => {
+    res.status(200).json({ message: "Server is Running" })
 })
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,12 @@ const { Server } = require("socket.io");
 const http = require("http");
 const cors = require("cors");
 const configSocketIO = require("./controllers/chat.controller");
-
-const app = express();
+const app = require("./app");
 
 // Express CORS
 app.use(cors({
-    origin: ["https://omee-chat-app.netlify.app/"],
+    origin: ["https://omee-chat-app.netlify.app/", "http:localhost:3000", "http:localhost:5173"],
+    origin: "*",
     methods: ["GET", "POST"]
 }));
 
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 // Socket CORS
 const io = new Server(server, {
     cors: {
-        origin: ["https://omee-chat-app.netlify.app/"],
+        origin: ["https://omee-chat-app.netlify.app/", "http:localhost:3000", "http:localhost:5173"],
         methods: ["GET", "POST"]
     }
 });
